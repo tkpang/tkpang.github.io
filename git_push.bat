6cb8b17c4101a9c8ffd8e9bd84cbb991a9e9c00e
tkpang <pangtiankai@gmail.com>
2023-07-10 10:30:31 +0800

    pangtiankai - 周一 2-23-07 10:30:31

--- git_push.bat
+++ git_push.bat
@@ -6,7 +6,7 @@ echo                                      GitHub自动上传脚本
 echo ===================================================================================
 echo.
 
-set branch=main
+set branch=master
 set change=pangtiankai
 echo.
 set datetime=%date:~0,4%-%date:~5,2%-%date:~8,2% %time:~0,2%:%time:~3,2%:%time:~6,2%
